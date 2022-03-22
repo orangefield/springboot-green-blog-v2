38a3b048858a0715202708ac4298f65fd340f60d
field <jsw_777@naver.com>
2022-03-22 12:36:41 +0900

    5. 회원가입 완료

---
+++ src/main/java/site/metacoding/blogv2/web/api/dto/user/JoinDto.java
@@ -0,0 +1,25 @@
+package site.metacoding.blogv2.web.api.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class JoinDto {
+    private String username;
+    private String password;
+    private String email;
+    private String addr;
+
+    public User toEntity() {
+        User user = new User();
+        user.setUsername(this.username);
+        user.setPassword(this.password);
+        user.setEmail(this.email);
+        user.setAddr(this.addr);
+        return user;
+    }
+}
